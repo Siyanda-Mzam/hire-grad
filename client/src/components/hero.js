46434a319675de141ee45f2ae3951c8d51cb0e4d
simzam <siyanda.mzam@hellocomputer.com>
2017-09-20 10:05:53 +0200

    Fixed loading bar

--- client/src/components/hero.js
+++ client/src/components/hero.js
@@ -9,14 +9,22 @@ const Hero = () => (
             <div className="column is-8">
               <h1 className="title">Lorem ipsum dolor sit amet, consectetur adipiscing elit</h1>
               <h2 className="is-10 subtitle">Quacumque enim ingredimur, in aliqua historia vestigium ponimus. Bonum valitudo: miser morbus. Familiares nostros, credo, Sironem dicis et Philodemum, cum optimos viros, tum homines doctissimos.</h2>
-              <div className="field has-addons">
-                <p className="control">
-                  <input type="text" className="input" placeholder="Email address" />
-                </p>
-                <p className="control">
-                  <a href="" className="button is-info">Get started</a>
-                </p>
-              </div>
+                <div className="field has-addons">
+                  <p className="control">
+                    <span className="select">
+                      <select>
+                        <option>I am a graduate</option>
+                        <option>I am an employer</option>
+                      </select>
+                    </span>
+                  </p>
+                  <p className="control">
+                    <input type="text" className="input" placeholder="Email address"/>
+                  </p>
+                  <p className="control">
+                    <a href="" className="button is-info">Get started</a>
+                  </p>
+                </div>
             </div>
             <div className="column pull-up">
               <img src={hireGrad} />
--- client/src/components/list_item_view.js
+++ client/src/components/list_item_view.js
@@ -11,7 +11,7 @@ class ListItemView extends Component {
   render() {
     const { item } = this.props;
     if (!item) {
-      return (<div><Loader/></div>);
+      return (<Loader/>);
     }
 
     return (
