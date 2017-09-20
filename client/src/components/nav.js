34ee955afdfd787ea8fdd9066cf6a350550e0459
Siyanda-Mzam <dot.netntoz@gmail.com>
2017-09-20 05:12:12 +0200

    WIP - Hero section

--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -2,15 +2,20 @@ import React from 'react';
 import logo from '../../res/images/logo.gif';
 
 const Nav = () => (
-  <div className="container navbar">
-      <nav className="is-pulled-right" data-scroll-element=".how-it-works">
-          <ul className="nav-items tabs">
-              <li className="nav-item is-size-5"><a href="/candidate/siyanda-mzam/dashboard">Graduate</a></li>
-              <li className="nav-item is-size-5"><a href="/candidate/siyanda-mzam">Employer</a></li>
-              <li className="nav-item is-size-5"><a href="/candidate/siyanda-mzam/interview_requests">Sign In</a></li>
-              <li className="nav-item is-size-5"><a className="navbar-item box" href="/candidate/siyanda-mzam/referrals?s=top_nav">Sign Up</a></li>
-          </ul>
-      </nav>
+  <div className="container">
+    <nav className="nav container">
+      <div className="nav-left">
+        <a href="" className="nav-item">
+          <h1 className="title is-4">Higher A Graduate</h1>
+        </a>
+      </div>
+      <div className="nav-right nav-menu">
+        <a href="#" className="nav-item">Graduate</a>
+        <a href="#" className="nav-item">Employer</a>
+        <a href="#" className="nav-item">Sign In</a>
+        <div className="nav-item"><a href="#" className="button is-info is-outlined">Sign Up</a></div>
+      </div>
+    </nav>
   </div>
 );
 
