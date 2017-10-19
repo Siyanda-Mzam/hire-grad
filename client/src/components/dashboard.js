55deed049fd1e1360c9d5a32ac5f4e4850abf775
PeachBots_20170602 <peachbots_20170602@PeachBots-20170602s-Mac-mini.local>
2017-10-19 16:41:10 +0200

    Added a loader for rendering while fetching data

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import fire from '../config/firebase';
 import Nav from './nav';
+import Loader from './loader';
 
 class Dashboard extends Component {
   constructor(props) {
@@ -25,7 +26,24 @@ class Dashboard extends Component {
     const props = this.props.history.location;
     if (!this.state.hasReceivedData) {
       return (
-        <div>Loading</div>
+        <div className="section"><div className="section"><Loader/></div>
+          <br/><br/><br/>
+          <br/><br/><br/>
+          <br/><br/><br/>
+          <br/><br/><br/>
+          <br/><br/><br/>
+          <br/><br/><br/>
+          <br/><br/><br/>
+          <br/><br/><br/>
+          <br/><br/><br/>
+          <br/><br/><br/>
+          <br/><br/><br/>
+          <br/><br/><br/>
+          <br/><br/><br/>
+          <br/><br/><br/>
+          <br/><br/><br/>
+          <br/><br/><br/>
+        </div>
       );
     }
     else {
@@ -33,6 +51,28 @@ class Dashboard extends Component {
         <div>
         <Nav navStyle={"sign-up-nav-background-color"}/>
         <br/><br/>
+        <div className="section">
+          <div className="container">
+            <div className="card">
+              <div className="card-content">
+                <div className="media">
+                  <div className="media-left">
+                    <figure className="is-4by3">
+                      <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
+                    </figure>
+                  </div>
+                  <div className="media-content">
+                    <p className="title is-4">{this.state.databaseSnapshot.name}</p>
+                    <p className="subtitle is-6"><i className="fa fa-map-marker"></i> Johannesburg</p>
+                    <p className="subtitle is-6"><i className="fa fa-graduation-cap"></i> Rhodes University</p>
+                    <p className="subtitle is-6" contentEditable="true"><i className="fa fa-money"></i> R34 000/month</p>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="section">
           <div className="container">
             <div className="card">
