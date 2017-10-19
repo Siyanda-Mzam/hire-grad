df100f8c6b35a6d8fdf58b127e619cc3292a54dc
PeachBots_20170602 <peachbots_20170602@PeachBots-20170602s-Mac-mini.local>
2017-10-19 16:55:55 +0200

    Tweaking the dashboard

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -95,6 +95,52 @@ class Dashboard extends Component {
             </div>
           </div>
         </div>
+
+
+        <div className="section">
+          <div className="container">
+            <div className="card">
+              <div className="card-content">
+                <div className="media">
+                  <div className="media-left">
+                    <figure className="is-4by3">
+                      <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
+                    </figure>
+                  </div>
+                  <div className="media-content">
+                    <p className="title is-4">{this.state.databaseSnapshot.name}</p>
+                    <p className="subtitle is-6"><i className="fa fa-map-marker"></i> Johannesburg</p>
+                    <p className="subtitle is-6"><i className="fa fa-graduation-cap"></i> Rhodes University</p>
+                    <p className="subtitle is-6" contentEditable="true"><i className="fa fa-money"></i> R34 000/month</p>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="section">
+          <div className="container">
+            <div className="card">
+              <div className="card-content">
+                <div className="media">
+                  <div className="media-left">
+                    <figure className="is-4by3">
+                      <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
+                    </figure>
+                  </div>
+                  <div className="media-content">
+                    <p className="title is-4">{this.state.databaseSnapshot.name}</p>
+                    <p className="subtitle is-6"><i className="fa fa-map-marker"></i> Johannesburg</p>
+                    <p className="subtitle is-6"><i className="fa fa-graduation-cap"></i> Rhodes University</p>
+                    <p className="subtitle is-6" contentEditable="true"><i className="fa fa-money"></i> R34 000/month</p>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
         </div>
       );
     }
