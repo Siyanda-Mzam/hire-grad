18a93fd9e4d2705ca5a6609de7fee2acf03bab8b
PeachBots_20170602 <peachbots_20170602@PeachBots-20170602s-Mac-mini.local>
2017-10-20 16:37:57 +0200

    Added the rest of the sections of the dashboard/candidate-profile. This is quite rough and will later be cleaned up and optimized

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -8,7 +8,9 @@ class Dashboard extends Component {
     super(props);
     this.state = {
       hasReceivedData: false,
-      databaseSnapshot: null
+      databaseSnapshot: null,
+      isEdit: false
+
     }
   }
   componentDidMount(props) {
@@ -23,9 +25,11 @@ class Dashboard extends Component {
     });
   }
   render() {
+
     const props = this.props.history.location;
     if (!this.state.hasReceivedData) {
       return (
+
         <div className="section"><div className="section"><Loader/></div>
           <br/><br/><br/>
           <br/><br/><br/>
@@ -48,10 +52,11 @@ class Dashboard extends Component {
     }
     else {
       return (
+
         <div>
         <Nav navStyle={"sign-up-nav-background-color"}/>
         <br/><br/>
-        <div className="section">
+        <div className="section head ">
           <div className="container">
             <div className="card">
               <div className="card-content">
@@ -63,18 +68,238 @@ class Dashboard extends Component {
                   </div>
                   <div className="media-content column is-7 center-xy-absolutely">
                     <p className="title is-4">{this.state.databaseSnapshot.name}</p>
-                    <p className=""><i className="fa fa-map-marker"></i> Johannesburg</p>
-                    <p className=""><i className="fa fa-graduation-cap"></i> Rhodes University</p>
-                    <p className=""><i className="fa fa-money"></i> R34 000/month</p>
+                    <p className="">
+                      <span className="icon "><i className="fa fa-map-marker"></i></span> Johannesburg
+                      <i className="fa fa-pencil push-right"></i>
+                    </p>
+                    <p className="">
+                      <span className="icon "><i className="fa fa-graduation-cap"></i></span>Rhodes University
+                      <i className="fa fa-pencil push-right"></i>
+                    </p>
+                    <p className="">
+                      <i className="fa fa-money"></i> R34 000/month
+                      <i className="fa fa-pencil push-right"></i>
+                    </p>
                   </div>
                 </div>
-
               </div>
             </div>
           </div>
         </div>
 
+        <div className="section summary ">
+          <div className="container">
+          <div className="card">
+            <header className="card-header">
+              <p className="card-header-title">
+                Summary
+              </p>
+              <a href="#" className="card-header-icon" aria-label="more options">
+                <span className="icon">
+                  <a>Edit</a>
+                </span>
+              </a>
+            </header>
+            <div className="card-content">
+              <div className="content">
+                <p className="title is-5">About me</p>
+                <p className="about_me">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.</p>
+                <p className="title is-5">How I stay on top of my game</p>
+                <p className="about_me">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.</p>
+                <p className="title is-5">The future of my career path</p>
+                <p className="about_me">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.</p>
+              </div>
+            </div>
+            <footer className="card-footer">
+              {this.state.isEdit ? <a href="#" className="card-footer-item">Save</a> : ''}
+            </footer>
+          </div>
+          </div>
         </div>
+
+        <div className="section roles-interestedin">
+          <div className="container">
+          <div className="card">
+            <header className="card-header">
+              <p className="card-header-title">
+                Roles I am interested in
+              </p>
+            </header>
+            <div className="card-content">
+              <div className="content">
+
+              </div>
+            </div>
+            <footer className="card-footer">
+              {this.state.isEdit ? <a href="#" className="card-footer-item">Save</a> : ''}
+            </footer>
+          </div>
+          </div>
+        </div>
+
+
+        <div className="section skills">
+          <div className="container">
+          <div className="card">
+            <header className="card-header">
+              <p className="card-header-title">
+                Skills
+              </p>
+            </header>
+            <div className="card-content">
+              <div className="content">
+                  <div className="tags">
+                    <span className="tag is-medium is-success">
+                      Numerical Methods
+                      <button className="delete is-small"></button>
+                    </span>
+                    <span className="tag is-medium is-success">
+                      Algebra
+                      <button className="delete is-small"></button>
+                    </span>
+                    <span className="tag is-medium is-success">
+                      Topology
+                      <button className="delete is-small"></button>
+                    </span>
+                    <span className="tag is-medium is-success">
+                      Number Theory
+                      <button className="delete is-small"></button>
+                    </span>
+                    <span className="tag is-medium is-success">
+                      Differential Geometry
+                      <button className="delete is-small"></button>
+                    </span>
+                    <span className="tag is-medium is-success">
+                      Fluid Dynamics
+                      <button className="delete is-small"></button>
+                    </span>
+                    <span className="tag is-medium is-success">
+                      Functional Analysis
+                      <button className="delete is-small"></button>
+                    </span>
+                    <span className="tag is-medium is-success">
+                      Probability
+                      <button className="delete is-small"></button>
+                    </span>
+                    <span className="tag is-medium is-success">
+                      Harmonic Analysis
+                      <button className="delete is-small"></button>
+                    </span>
+                    <span className="tag is-medium is-success">
+                      Stochastic Calculus
+                      <button className="delete is-small"></button>
+                    </span>
+                  </div>
+              </div>
+            </div>
+            <footer className="card-footer">
+              {this.state.isEdit ? <a href="#" className="card-footer-item">Save</a> : ''}
+            </footer>
+          </div>
+          </div>
+        </div>
+
+        <div className="section roadmap">
+          <div className="container">
+          <div className="card">
+            <header className="card-header">
+              <p className="card-header-title">
+                Roadmap
+              </p>
+            </header>
+            <div className="card-content">
+              <div className="content">
+              <section id="conference-timeline">
+                <div className="timeline-start">Start</div>
+                  <div className="conference-center-line"></div>
+                  <div className="conference-timeline-content">
+                    <div className="timeline-article">
+                      <div className="content-left-container">
+                        <div className="content-left">
+                          <p>
+                            When I orbited the Earth in a spaceship, I saw for
+                            the first time how beautiful our planet is. Mankind,
+                            let us preserve and increase this beauty, and not
+                            destroy it!
+                            <span className="article-number">01</span>
+                          </p>
+                        </div>
+                      </div>
+                      <div className="content-right-container">
+                        <div className="content-right">
+                          <p>
+                            When I orbited the Earth in a spaceship, I saw for
+                            the first time how beautiful our planet is.
+                            <span className="article-number">02</span>
+                          </p>
+                        </div>
+                      </div>
+                      <div className="meta-date">
+                        <span className="date">2014</span>
+                      </div>
+                    </div>
+
+                    <div className="timeline-article">
+                      <div className="content-left-container">
+                        <div className="content-left">
+                          <p>
+                            When I orbited the Earth in a spaceship, I saw for
+                            the first time how beautiful our planet is. Mankind,
+                            let us preserve and increase this beauty, and not
+                            destroy it!
+                            <span className="article-number">01</span>
+                          </p>
+                        </div>
+                      </div>
+                      <div className="content-right-container">
+                        <div className="content-right">
+                          <p>When I orbited the Earth in a spaceship, I saw for
+                          the first time how beautiful our planet is.
+                          <span className="article-number">02</span></p>
+                        </div>
+                      </div>
+                      <div className="meta-date">
+                        <span className="date">2015</span>
+                      </div>
+                    </div>
+
+                    <div className="timeline-article">
+                      <div className="content-left-container">
+                        <div className="content-left">
+                        <p>
+                          When I orbited the Earth in a spaceship, I saw for
+                          the first time how beautiful our planet is. Mankind,
+                          let us preserve and increase this beauty, and not
+                          destroy it!
+                          <span className="article-number">01</span>
+                        </p>
+                        </div>
+                      </div>
+                      <div className="content-right-container">
+                        <div className="content-right">
+                          <p>
+                            When I orbited the Earth in a spaceship, I saw for
+                            the first time how beautiful our planet is.
+                            <span className="article-number">02</span></p>
+                        </div>
+                      </div>
+                      <div className="meta-date">
+                        <span className="date">2016</span>
+                      </div>
+                    </div>
+                  </div>
+                <div className="timeline-end">End</div>
+              </section>
+            </div>
+          </div>
+          <footer className="card-footer">
+            {this.state.isEdit ? <a href="#" className="card-footer-item">Save</a> : ''}
+          </footer>
+        </div>
+
+      </div>
+    </div>
+  </div>
       );
     }
   }
